7c5b17520e9d48b1ad616f894dd1b3eedca5982b
shlomo <shlomo.gevim@gmail.com>
2020-02-29 15:59:16 +0200

    Base

---
+++ app/src/main/java/com/example/yhaa37/Const.kt
@@ -0,0 +1,19 @@
+package com.example.yhaa37
+
+class Const {
+    companion object {
+        const val ASSEETS_FILE = "23"
+        const val TALKLIST = "talklist23"
+        const val PREFS_NAME = "myPrefs23"
+
+        const val FILE_NUM = "file_num30"
+      // const val PREFS_NAME = "myPrefs"
+        const val CURRENT_PAGE = "current_page"
+        const val LAST_PAGE = "last_talker"
+        const val LASTTALKER = "lasttalker"
+        const val SHOWPOSITION="show_position"
+
+        const val INTERVAL="interval"
+
+    }
+}
--- app/src/main/java/com/example/yhaa37/MainActivity.kt
+++ app/src/main/java/com/example/yhaa37/MainActivity.kt
@@ -1,12 +1,85 @@
 package com.example.yhaa37
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.helper_view_layout.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var animationInAction: AnimationInAction
+    lateinit var pref: GetAndStoreData
+    lateinit var arrangeScreen: ArrangeScreen
+    lateinit var buttonSpace: ButtonSpace
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAll()
+        enterData()
+        animationInAction.executeTalker()
     }
+
+
+    private fun enterData(){
+        //       pref.saveCurrentPage(1)
+//        val currentPage=pref.getCurrentPage()
+//        talkList[currentPage].animNum=1000
+//        pref.saveTalkingListInPref(talkList)
+    }
+
+    private fun initAll() {
+        pref = GetAndStoreData(this)
+
+        var showPosition = true
+        pref.saveShowPosition(showPosition)
+        pref.saveCurrentPage(1)
+
+        if (showPosition){
+            showPositionBtn.text="toTest"
+        }else{
+            showPositionBtn.text="toShow"
+        }
+
+        var  talkList = pref.getTalkingList(0)
+        arrangeScreen = ArrangeScreen(this)
+        buttonSpace = ButtonSpace(this)
+        animationInAction = AnimationInAction(this)
+        buttonSpace.initButton()
+        arrangeScreen.setLayoutShowMode()
+        arrangeScreen.operateListView()
+        buttonSpace.setShowPositionMode()
+    }
+
 }
+
+
+/* private fun getTalkList() {
+     talkList = pref.createNewList()
+
+     if (talkList.size == 0) {
+         // createTalkingListFromFirestore()  //open tool->firebase->firestore see if all depen. ok
+         //rebuilt project
+         // run and wait for result
+     }
+     if (talkList.size == 0) {
+         // !! must be in remarked becaseus it inteferring to the firebase
+         talkList = pref.createTalkListFromTheStart()
+
+     }
+     pref.saveTalkingListInPref(talkList)
+ }*/
+
+/*@SuppressLint("RestrictedApi")
+private fun waitToAnnimateEnded() {
+    Utile.listener1 = { _, _ ->
+        fab.visibility = View.VISIBLE
+        fab1.visibility = View.VISIBLE
+        ViewAnimator
+            .animate(fab)
+            .alpha(0f, 1f)
+            .andAnimate(fab1)
+            .alpha(0f, 1f)
+            .duration(3500)
+            .start()
+    }
+}*/
+
